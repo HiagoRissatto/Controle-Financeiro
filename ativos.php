bf1fffc2eb04f1cf727750ae0d095615d6a1e225
Hiago Rissatto <levexhiago@gmail.com>
2026-03-10 22:12:54 -0300

    Adiciona Bootstrap ao layout, refatora tabelas e ajusta cabeçalho para linkar corretamente ao relatório de ativos

--- ativos.php
+++ ativos.php
@@ -7,27 +7,37 @@ $relatorio = $ativo->calcularPrecoMedio();
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relatório de Ativos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <h1>Relatório de Ativos</h1>
-    <table border="1">
+    <?php include 'partials/header.php'; ?>
+    <h1 class="text-center ">Relatório de Ativos</h1>
+    <table class="table table-striped" border="1">
         <tr>
-            <th>Ativo</th>
-            <th>Total Comprado</th>
-            <th>Preço Médio</th>
+            <th scope="col">Ativo</th>
+            <th scope="col">Total Comprado</th>
+            <th scope="col">Preço Médio</th>
         </tr>
         <?php foreach ($relatorio as $linha): ?>
-            <tr>
-                <td><?= $linha['ativo'] ?></td>
-                <td><?= $linha['total_quantidade'] ?></td>
-                <td><?= number_format($linha['preco_medio'], 2, ',', '.') ?></td>
-            </tr>
+        <tr class="table-row">
+            <td><?= $linha['ativo'] ?></td>
+            <td><?= $linha['total_quantidade'] ?></td>
+            <td><?= number_format($linha['preco_medio'], 2, ',', '.') ?></td>
+        </tr>
         <?php endforeach; ?>
     </table>
+    <?php include 'partials/footer.php'; ?>
 </body>
+
 </html>
--- funcionario.php
+++ funcionario.php
@@ -1,38 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
     <title>Funcionários</title>
 </head>
+
 <body>
     <?php include 'partials/header.php'; ?>
-    <h1>Lista de Funcionários</h1>
+    <h1 class="text-center">Lista de Funcionários</h1>
     <?php
     require_once 'classes/Cadastro.php';
     $cadastro = new Cadastro();
     $funcionarios = $cadastro->listarFuncionarios();
     ?>
-    <div class="container">
-        <div class="tabela">
-            <table  border="2">
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Cargo</th>
-                </tr>
+            <div class="container">
+                <table class="table table-dark" border="1">
+                    <tr >
+                        <th class="col-md-4 text-center">Nome</th>
+                        <th class="col-md-4 text-center">Email</th>
+                        <th class="col-md-4 text-center">Cargo</th>
+                    </tr>
+                    <?php foreach ($funcionarios as $funcionario): ?>
+                    <tr>
+                        <td class="col-md-4 text-center"><?= $funcionario['nome'] ?></td>
+                        <td class="col-md-4 text-center"><?= $funcionario['email'] ?></td>
+                        <td class="col-md-4 text-center"><?= $funcionario['cargo'] ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
         
-            <?php foreach ($funcionarios as $funcionario): ?>
-                <tr>
-                    <td><?= $funcionario['nome'] ?></td>
-                    <td><?= $funcionario['email'] ?></td>
-                    <td><?= $funcionario['cargo'] ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        </div>
-    </div>
     <?php include 'partials/footer.php'; ?>
 </body>
+
 </html>
